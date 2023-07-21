6f74fcd0f08b119e7e2f6ca60d3564339260bcba
keyur <savanikeyur1011@gmail.com>
2023-07-21 09:12:18 +0530

    Final commit

---
+++ lib/API_With_StateManagement/services/post_method_service.dart
@@ -0,0 +1,24 @@
+
+import 'dart:convert';
+
+import 'package:flutter_rest_api/API_With_StateManagement/model/post_method_model.dart';
+import 'package:http/http.dart' as http;
+
+class PostMethodService{
+  Future<PostMethodModel> getPost(String title) async {
+    late PostMethodModel data;
+    final response = await http.post(
+        Uri.parse("https://jsonplaceholder.typicode.com/albums"),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'title': title,
+      }),
+    );
+    final body =response.body;
+    final json = jsonDecode(body);
+    final reasult = json;
+    return PostMethodModel.fromJson(reasult);
+  }
+}
